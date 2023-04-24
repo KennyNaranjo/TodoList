6f65c55b0553d96fe37a2e028ceaf3f14640a185
Kenny <kenny.naranjo.castro@gmail.com>
2023-04-24 23:41:57 +0200

    final commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const moongose = require('mongoose');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+//use express.json() para obtener data en formato json
+app.use(express.json());
+
+//Puerto
+
+const PORT = process.env.PORT || 5500;
+
+app.use(cors());
+
+//Importamos rutas
+const router = require('./routes/todoItems');
+
+//Vamos a conectar a MooongoDB...
+
+moongose.connect(process.env.DB_CONNECT)
+.then(()=> console.log("Data Connected"))
+.catch(err => console.log(err))
+
+app.use('/', router);
+
+
+//añadir puerto y conectar al servidor
+
+app.listen(PORT, ()=> console.log("Server Connected") );
---
+++ server/routes/todoItems.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+
+//IMPORTAMOS MODELO (OBJETO)
+const TodoItemModel = require('../models/todoItems');
+//CREAMOS LA PRIMERA RUTA -- AÑADIREMOS WISH ITEMS A LA DATABASE
+
+router.post('/api/item', async (req, res) => {
+    try {
+        const newItem = new TodoItemModel({
+            item: req.body.item
+        })
+        //GUARDAMOS EL OBJETO EN LA DATABASE
+        const saveItem = await newItem.save()
+        res.status(200).json(saveItem);
+
+    } catch (err) {
+        res.json(err);
+    }
+
+    //CREAMOS UNA SEGUNDA RUTA --OBTENEMOS DATA DE DATABASE
+
+router.get('/api/items', async (req, res) => {
+
+    try {
+        const allTodoItems = await TodoItemModel.find({});
+
+        res.status(200).json(allTodoItems)
+    } catch (err) {
+        res.json(err);
+
+    }
+
+})
+
+//UPDATEAMOS ITEM
+router.put('/api/item/:id', async (req, res) => {
+
+    try {
+        const updateItem = await TodoItemModel.findByIdAndUpdate(req.params.id, { $set: req.body })
+        res.status(200).json('Item Updated');
+    } catch (err) {
+        res.json(err);
+
+    }
+})
+// BORRAMOS ITEM DE LA DATABASE,
+
+router.delete('/api/item/:id', async (req, res) => {
+
+    try {
+        //encontramos el item por id y lo borramos
+        const deleteItem = await TodoItemModel.findByIdAndDelete(req.params.id);
+        res.status(200).json('Item Deleted');
+    } catch (err) {
+        res.json(err);
+    }
+})
+
+//EXPORT ROUTER
+})
+module.exports = router;
+
